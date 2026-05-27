31a3b11be381a914835a7996501dea5347b2852c
sborisbo <boris.leibman@gmail.com>
2026-05-27 14:32:59 +0300

    Create feeds.py

---
+++ feeds.py
@@ -0,0 +1,6 @@
+RSS_FEEDS = [
+    "https://feeds.feedburner.com/venturebeat/SZYF",
+    "https://techcrunch.com/feed/",
+    "https://www.artificialintelligence-news.com/feed/",
+    "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
+]
